5e0b3fb48ef5d193d89dbea620e0e6d1aaffa8fd
Vergervan <vergervan@github.com>
2022-10-30 23:55:37 +0300

    Test

--- backend/src/appnetworkhandler.cpp
+++ backend/src/appnetworkhandler.cpp
@@ -5,6 +5,7 @@ void AppNetworkHandler::handleData(qintptr socketDscr, QString msg)
     QTcpSocket clientSocket;
     clientSocket.setSocketDescriptor(socketDscr);
 
+
     QJsonDocument doc;
     doc.fromJson(QByteArray::fromStdString(msg.toStdString()));
 }
